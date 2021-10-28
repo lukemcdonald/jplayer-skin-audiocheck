3df4baad934fe5bd9c7a9159245a8b76400b2083
Luke McDonald <thelukemcdonald@gmail.com>
2021-10-28 10:38:05 -0500

    Fix issue where player is not visible after scrolling playlist.
    After selecting the "Scroll" display and scrolling down, the player gets hidden when the "Hide" playlist is then selected.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,14 +6,20 @@ const app = {
   setupPlaylistDisplay: () => {
     const player = document.querySelector('.audio-player')
     const toggles = document.querySelectorAll('.playlist-toggles button')
+    const jpAudio = document.querySelector('.jp-audio')
 
     function toggleOpen(event) {
       const current = Array.from(toggles).find(toggle => toggle.classList.contains('selected'))
       const selected = event.target
 
+      // Set audio player scroll back to top to ensure playlist is visible.
+      jpAudio.scrollTop = 0
+
+      // Update selected item classes.
       current.classList.remove('selected')
       selected.classList.add('selected')
 
+      // Update playlist display class.
       player.classList.remove(`playlist-${current.getAttribute('data-display')}`)
       player.classList.add(`playlist-${selected.getAttribute('data-display')}`)
     }
