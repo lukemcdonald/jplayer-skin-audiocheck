ffe9b7a9c68332fba65892bfb4aa7af3c0fa8e00
Luke McDonald <lukemcdonald@users.noreply.github.com>
2024-01-20 08:51:57 -0600

    chore: remove prettier plugin dependencies (#16)
    Co-authored-by: Luke McDonald <lukemcdonald@Lukes-M1.local>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [`plugin:prettier/recommended`],
-  plugins: ['prettier'],
+  extends: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
-  },
-  rules: {
-    'prettier/prettier': 'error',
   },
 }
